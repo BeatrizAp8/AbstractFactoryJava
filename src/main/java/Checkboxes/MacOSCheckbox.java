1f2572c9fa31b3bfd4f118383206757ca0639c21
Beatriz Aparecida <125601034+BeatrizAp8@users.noreply.github.com>
2025-03-22 16:21:13 -0300

    MacOSCheckbox.java

--- src/main/java/Checkboxes/MacOSCheckbox.java
+++ src/main/java/Checkboxes/MacOSCheckbox.java
@@ -3,16 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Checkboxes;
-
+/**
+ * A classe MacOSCheckbox é uma implementação concreta da interface Checkbox.
+ * Ela representa uma caixa de seleção (checkbox) específica para o sistema operacional MacOS.
+ * Essa classe faz parte do padrão **Abstract Factory**, que permite criar famílias de objetos
+ * relacionados (como botões, checkboxes, etc.) sem especificar suas classes concretas.
+ * 
+ * A implementação de uma caixa de seleção (checkbox) para o MacOS irá ser diferente de outras plataformas, 
+ * como o Windows. No contexto do Abstract Factory, o foco é garantir que as implementações de UI 
+ * (como o MacOSCheckbox) sigam o estilo visual específico da plataforma.
+ *  */
 /**
  *
  * @author FATEC ZONA LESTE
  */
 public class MacOSCheckbox implements Checkbox {
-
+ /**
+     * Construtor da classe MacOSCheckbox.
+     * A exceção UnsupportedOperationException foi gerada automaticamente,
+     * indicando que o construtor não está implementado. Em uma implementação real,
+     * o construtor poderia inicializar recursos específicos necessários para o funcionamento
+     * da caixa de seleção (checkbox) no MacOS.
+     */
     public MacOSCheckbox() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    /**
+     * Implementação do método paint() da interface Checkbox.
+     * Este método é responsável por exibir uma mensagem indicando que uma caixa de seleção 
+     * MacOS foi criada. Em um cenário real, o método "pintaria" a caixa de seleção na interface gráfica
+     * com o estilo visual característico do MacOS.
+     * 
+     * No padrão Abstract Factory, essa implementação seria diferente para outras plataformas.
+     * Por exemplo, uma classe WindowsCheckbox teria um estilo visual diferente para a caixa de seleção.
+     */
 
     @Override
     public void paint() {
