f557509273d64711805c617efb3d4079cfc09e21
Beatriz Aparecida <125601034+BeatrizAp8@users.noreply.github.com>
2025-03-21 22:20:41 -0300

    Update Demo.java

--- src/main/java/App/Demo.java
+++ src/main/java/App/Demo.java
@@ -22,20 +22,28 @@ public class Demo {
 // Método de configuração da aplicação que cria uma instância de Application
 // com a fábrica apropriada com base no sistema operacional.
     private static Application configureApplication() {
-        Application app;
-        GUIFactory factory;
+        Application app; // Variável para armazenar a aplicação configurada
+        GUIFactory factory; // Fábrica que criará os componentes gráficos (botões, checkboxes)
+// Obtém o nome do sistema operacional onde a aplicação está sendo executada
         String osName = System.getProperty("os.name").toLowerCase();
+// Verifica se o sistema operacional é MacOS. Caso seja, cria uma fábrica específica para MacOS.
         if (osName.contains("mac")) {
-            factory = new MacOSFactory();
+            factory = new MacOSFactory();  // Cria uma fábrica para o MacOS
         } else {
-            factory = new WindowsFactory();
+            factory = new WindowsFactory();  // Caso contrário, cria uma fábrica para o Windows
         }
+     // Cria a aplicação, passando a fábrica concreta (MacOSFactory ou WindowsFactory)
+    // para que a aplicação tenha acesso aos componentes gráficos corretos para o sistema operacional.
         app = new Application(factory);
+    // Retorna a aplicação configurada com a fábrica escolhida
         return app;
     }
 
     public static void main(String[] args) {
+// Configura a aplicação com a fábrica apropriada dependendo do sistema operacional
         Application app = configureApplication();
+ 
+// Chama o método 'paint' para desenhar ou exibir os componentes gráficos da aplicação
         app.paint();
     }
 }
